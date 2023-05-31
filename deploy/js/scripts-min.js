1d889ecff69e6be15309f104822e02f30c06f3fe
shannonwoods814 <sew347@drexel.edu>
2023-05-30 21:08:45 -0400

    updates 5/30/23
    end of class

---
+++ deploy/js/scripts-min.js
@@ -0,0 +1,20 @@
+let burgerBtn = document.querySelector("#burger-btn");
+let mainMenu = document.querySelector("#main-menu");
+
+let canSeeMenu = false;
+
+burgerBtn.addEventListener("click", () => {
+
+    if(!canSeeMenu){
+        mainMenu.classList.add("show-menu");
+        canSeeMenu = true;
+    }else{
+        mainMenu.classList.remove("show-menu");
+        canSeeMenu = false;
+    }
+})
+
+mainMenu.addEventListener("click", () => {
+        mainMenu.classList.remove("show-menu");
+        canSeeMenu = false;
+})
